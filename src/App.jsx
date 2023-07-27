438ee2cfec1598b42a1ad7c22304c54e1d19400c
Shmuel Tufahi <tufas29@gmail.com>
2023-07-27 08:31:12 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import "./App.css"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import OtherPage from "./OtherPage"
+import Fib from "./Fib.jsx"
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+  )
+}
+
+export default App
